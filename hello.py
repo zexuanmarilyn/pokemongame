5d698d986df3ff5d9b334c1d7f49cebbd03cb932
Ze Xuan <memenlolol@gmail.com>
2020-08-14 12:29:28 +0800

    Lists

--- hello.py
+++ hello.py
@@ -3,12 +3,14 @@ import requests
 r = requests.get("https://google.com")
 print(r.status_code)
 
-name=input("Your name? ")
-print("Hello,",name)
+
 print(r.ok)
-
-
 
 list_of_ppl=["Wayne", "Esther"]
 list_of_ppl.append("Ze Xuan")
 print(list_of_ppl)
+
+list2=[]
+for i in range(10):
+    list2.append(i)
+print(list2)
