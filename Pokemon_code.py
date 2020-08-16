a829796d422d926970790089f25d0260c2d92e41
Ze Xuan <memenlolol@gmail.com>
2020-08-16 20:35:08 +0800

    saved

--- Pokemon_code.py
+++ Pokemon_code.py
@@ -9,23 +9,36 @@ class Pokemon:
     
     def lose_health(self, current_health, health_lost):
         self.current_health=current_health - health_lost
-        print(("{} now has {} health").format(self.name, self.current_health))
+        return print(("{} now has {} health").format(self.name, self.current_health))
     
     def gain_health(self, current_health, health_gained, max_health):
         self.current_health = max( (current_health + health_gained), max_health)
-        print(("{} now has {} health").format(self.name, self.current_health))
+        return print(("{} now has {} health").format(self.name, self.current_health))
     
-    fire_dictionary={'type':'fire', 'weakness':'water', 'strength':'grass'}
-    water_dictionary={'type':'water', 'weakness':'grass', 'strength':'fire'}
-    grass_dictionary={'type':'grass', 'weakness':'fire', 'strength':'water'}
-    type_list= [fire_dictionary, water_dictionary, grass_dictionary]
+    #for question 3, create variables for knocking out Pokemon and reviving pokemons
+
+
 
     def attack (self, other_pokemon, damage):
+        fire_dictionary={'type':'fire', 'weakness':'water', 'strength':'grass'}
+        water_dictionary={'type':'water', 'weakness':'grass', 'strength':'fire'}
+        grass_dictionary={'type':'grass', 'weakness':'fire', 'strength':'water'}
+        type_list= [fire_dictionary, water_dictionary, grass_dictionary]
         for dictionary in type_list:
             if self.type == dictionary.get(type):
-                self.strength = dictionary.get(strength)
-                self.weakness = dictionary.get(weakness)
+                self.strength = dictionary.get("strength")
+                self.weakness = dictionary.get("weakness")
         if self.strength == other_pokemon.type:
-            damage = 
+            damage = other_pokemon.level*2
+        elif self.weakness == other_pokemon.type:
+            damage = other_pokemon.level*.5
+        else:
+            damage = other_pokemon.level
+        other_pokemon.lose_health(damage)
+        return print(("Your pokemon has been attacked and lost {} health").format(damage))
+
+    #before continuing, test out the above functions with some examples and debug if needed
+
+    
 
 
